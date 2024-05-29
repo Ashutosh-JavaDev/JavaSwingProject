2662ba220897aa816777ed763e1eb0e78acb7baa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-29 13:55:04 +0530

    Bank Project

--- BankManagementSystem/src/LoginPage.java
+++ BankManagementSystem/src/LoginPage.java
@@ -4,29 +4,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class LoginPage extends JFrame {
-    public LoginPage(){
+    public LoginPage() {
         setTitle("ATM Machine");
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icons/credit-cards-payment.png"));
-        Image i2=i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel label=new JLabel(i3);
-        label.setBounds(50,10,100,100);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/credit-cards-payment.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        label.setBounds(50, 10, 100, 100);
         add(label);
-        JLabel card=new JLabel("Card No.");
-        card.setBounds(50,120,70,30);
-        JLabel Pin=new JLabel("Pin No.");
-        Pin.setBounds(50,200,70,30);
-        JTextField cardField=new JTextField();
-        cardField.setBounds(120,120,150,30);
-        JTextField pinField=new JTextField();
-        pinField.setBounds(120,200,150,30);
-        JButton sign=new JButton("Sign IN");
-        sign.setBounds(120,250,70,30);
+        JLabel card = new JLabel("Card No.");
+        card.setBounds(50, 120, 70, 30);
+        JLabel Pin = new JLabel("Pin No.");
+        Pin.setBounds(50, 200, 70, 30);
+        JTextField cardField = new JTextField();
+        cardField.setBounds(120, 120, 150, 30);
+        JTextField pinField = new JTextField();
+        pinField.setBounds(120, 200, 150, 30);
+        JButton sign = new JButton("Sign IN");
+        sign.setBounds(120, 250, 70, 30);
         sign.setBackground(Color.black);
         sign.setForeground(Color.white);
         add(sign);
-        JButton login=new JButton("Log IN");
-        login.setBounds(180,250,70,30);
+        JButton login = new JButton("Log IN");
+        login.setBounds(180, 250, 70, 30);
         login.setBackground(Color.black);
         login.setForeground(Color.white);
         add(login);
@@ -34,9 +34,9 @@ public class LoginPage extends JFrame {
         add(pinField);
         add(cardField);
         add(Pin);
-        setLocation(400,200);
+        setLocation(400, 200);
         setLayout(null);
-        setSize(800,400);
+        setSize(800, 400);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
