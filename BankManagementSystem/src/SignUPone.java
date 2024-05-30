5abb52c6b80ce1eeb47034c74d150e852306475c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-30 12:21:59 +0530

    Bank Project

--- BankManagementSystem/src/SignUPone.java
+++ BankManagementSystem/src/SignUPone.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
-public class SignUPone {
-    
+public class SignUPone extends JFrame implements ActionListener {
+    JButton next;
+    public SignUPone(){
+        next=new JButton("NEXT");
+        next.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent ae){
+        
+    }
 }
