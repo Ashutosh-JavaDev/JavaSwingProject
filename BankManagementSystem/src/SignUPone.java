2b745dd2308e36d0dde1ccedd6b3d52056b8083d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-30 12:24:57 +0530

    Bank Project

--- BankManagementSystem/src/SignUPone.java
+++ BankManagementSystem/src/SignUPone.java
@@ -10,7 +10,9 @@ public class SignUPone extends JFrame implements ActionListener {
         next.addActionListener(this);
         setLayout(null);
         setSize(800,850);
-        getContentPane().setBackground(Color.white);;
+        getContentPane().setBackground(Color.white);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public void actionPerformed(ActionEvent ae){
 
