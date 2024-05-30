0bd93c8f1a2221368590472a7d552a2197f18f2d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-30 12:46:13 +0530

    Bank Project

--- BankManagementSystem/src/SignUPone.java
+++ BankManagementSystem/src/SignUPone.java
@@ -2,22 +2,26 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
-public class SignUPone extends JFrame implements ActionListener {
-    JLabel FirstName,LastName,FatherName,DOB,Gender,Email,Maratial,Address,City,State,Pin;
-    JTextField FirstTextField,LastTextField,DOBTextField,GenderTextField,EmailTextField,MaratialTextField,AddressTextField,CityTextField,StateTextField,PinTextField;
-    JButton next;
-    public SignUPone(){
 
-        next=new JButton("NEXT");
+public class SignUPone extends JFrame implements ActionListener {
+    JLabel FirstName, LastName, FatherName, DOB, Gender, Email, Maratial, Address, City, State, Pin;
+    JTextField FirstTextField, LastTextField, DOBTextField, GenderTextField, EmailTextField, MaratialTextField,
+            AddressTextField, CityTextField, StateTextField, PinTextField;
+    JButton next;
+
+    public SignUPone() {
+
+        next = new JButton("NEXT");
         next.setFont(new Font("Arial", Font.BOLD, 15));
         next.addActionListener(this);
         setLayout(null);
-        setSize(800,850);
+        setSize(800, 850);
         getContentPane().setBackground(Color.white);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
 }
