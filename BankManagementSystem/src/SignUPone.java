d6a9030ce18aa0928bfd8eab199375f706ce92c2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-30 12:45:46 +0530

    Bank Project

--- BankManagementSystem/src/SignUPone.java
+++ BankManagementSystem/src/SignUPone.java
@@ -3,8 +3,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
 public class SignUPone extends JFrame implements ActionListener {
+    JLabel FirstName,LastName,FatherName,DOB,Gender,Email,Maratial,Address,City,State,Pin;
+    JTextField FirstTextField,LastTextField,DOBTextField,GenderTextField,EmailTextField,MaratialTextField,AddressTextField,CityTextField,StateTextField,PinTextField;
     JButton next;
     public SignUPone(){
+
         next=new JButton("NEXT");
         next.setFont(new Font("Arial", Font.BOLD, 15));
         next.addActionListener(this);
