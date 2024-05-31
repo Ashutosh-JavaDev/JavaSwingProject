dbb9e7bfdd9c9d542dc304be955d5cf39c27a4e5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-31 13:26:51 +0530

    Bank Project

--- BankManagementSystem/src/SignUptwo.java
+++ BankManagementSystem/src/SignUptwo.java
@@ -2,6 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-public class SignUptwo {
-    
+public class SignUptwo extends JFrame implements ActionListener{
+    public SignUptwo(String formno){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        
+    }
 }
