fe5c7f39e08fb2d66753360dd2945f4ecceeb2c6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-01 15:46:14 +0530

    Bank Project

--- BankManagementSystem/src/SignUpthree.java
+++ BankManagementSystem/src/SignUpthree.java
@@ -2,18 +2,19 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.random.RandomGenerator;
+
 public class SignUpthree extends JFrame implements ActionListener {
-    JRadioButton saving,current,fixed,Recurring;
-    JCheckBox ATM,internet,mobile, email,cheque,statement;
-
-    JButton submit,cancel;
+    JRadioButton saving, current, fixed, Recurring;
+    JCheckBox ATM, internet, mobile, email, cheque, statement;
+    JButton submit, cancel;
     String formNo;
-    public SignUpthree(String formNo){
-        this.formNo=formNo;
 
-    }   
-    public void actionPerformed(ActionEvent ae){
+    public SignUpthree(String formNo) {
+        this.formNo = formNo;
+
+    }
+
+    public void actionPerformed(ActionEvent ae) {
         new SignUpthree("");
-    } 
+    }
 }
-
