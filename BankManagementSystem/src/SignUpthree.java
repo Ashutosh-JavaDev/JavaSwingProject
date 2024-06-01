6cf06e3f8172aa24f87c694c8debf5d3f68ad3b6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-01 16:14:03 +0530

    Bank Project

--- BankManagementSystem/src/SignUpthree.java
+++ BankManagementSystem/src/SignUpthree.java
@@ -16,14 +16,31 @@ public class SignUpthree extends JFrame implements ActionListener {
         account.setBounds(280,25,400,30);
         account.setFont(new Font("Raleway",Font.BOLD,20));
         add(account);
+        // Account
         JLabel type=new JLabel("Account Type");
         type.setBounds(100,80,100,20);
         type.setFont(new Font("Raleway",Font.BOLD,20));
         add(type);
+        // Saving Button
         saving=new JRadioButton("Saving Account");
         saving.setBounds(100,90,100,20);
         saving.setFont(new Font("Raleway",Font.BOLD,15));
         add(saving);
+         // Fixed Button
+         fixed=new JRadioButton("Fixed Account");
+         fixed.setBounds(150,90,100,20);
+         fixed.setFont(new Font("Raleway",Font.BOLD,15));
+         add(fixed); 
+         // Current Button
+         current=new JRadioButton("Current Account");
+         current.setBounds(100,130,100,20);
+         current.setFont(new Font("Raleway",Font.BOLD,15));
+         add(current); 
+         // Deposite Button
+         Recurring=new JRadioButton("Reccuring Deposite Account");
+         Recurring.setBounds(150,130,200,20);
+         Recurring.setFont(new Font("Raleway",Font.BOLD,15));
+         add(Recurring);
         //Default Information
         setLayout(null);
         setLocation(450, 100);
