57278b0d9be8898a673c1dfad890b90a59935bd1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-01 15:34:53 +0530

    Bank Project

--- BankManagementSystem/src/SignUpthree.java
+++ BankManagementSystem/src/SignUpthree.java
@@ -2,6 +2,12 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.random.RandomGenerator;
-public class SignUpthree {
-    
+public class SignUpthree extends JFrame implements ActionListener {
+    public SignUpthree(String formNo){
+
+    }   
+    public void actionPerformed(ActionEvent ae){
+        
+    } 
 }
+
