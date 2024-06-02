433955d7bc1f8cdc6abc66b66d851bde5b808b8b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-02 19:53:23 +0530

    Bank Project

--- BankManagementSystem/src/Transicition.java
+++ BankManagementSystem/src/Transicition.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class Transicition {
+public class Transicition extends JFrame implements ActionListener {
     public Transicition(){
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
 
     }
     public static void main(String[]args){
