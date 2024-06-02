530c1e14bdd165cdfbf5e126c45855f43712a709
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-02 20:04:50 +0530

    Bank Project

--- BankManagementSystem/src/Transicition.java
+++ BankManagementSystem/src/Transicition.java
@@ -4,7 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Transicition extends JFrame implements ActionListener {
     public Transicition(){
-
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icon/ATM.jpg"));
+        Image i2=i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel imagelabel=new JLabel(i3);
+        imagelabel.setBounds(900,900,0,0);
+        add(imagelabel);
         setLayout(null);
         getContentPane().setBackground(Color.white);
         setSize(900,900);
