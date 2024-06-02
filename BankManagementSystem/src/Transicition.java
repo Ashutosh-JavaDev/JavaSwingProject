716e99096c04cac602b09c2f9e5088d9e2772b4c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-02 19:52:08 +0530

    Bank Project

--- BankManagementSystem/src/Transicition.java
+++ BankManagementSystem/src/Transicition.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Transicition {
     public Transicition(){
-        
+
+    }
+    public static void main(String[]args){
+        new Transicition();
     }
 }
