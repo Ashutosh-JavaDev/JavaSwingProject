e46cc73c447adcba6317327a660e8c576e2cac04
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-04 21:42:37 +0530

    Bank Project

--- BankManagementSystem/src/Deposite.java
+++ BankManagementSystem/src/Deposite.java
@@ -10,14 +10,18 @@ public class Deposite extends JFrame implements ActionListener {
 
     public Deposite(String pinnumber) {
         this.pinnumber = pinnumber;
-        JLabel text=new JLabel("Plese enter the Amount want to Deposit");
-        setBounds(155,300,400,25);
-        setFont(new Font("System",Font.BOLD,16));
-        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/image.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        JLabel text = new JLabel("Plese enter the Amount want to Deposit");
+        setBounds(155, 300, 400, 25);
+        setFont(new Font("System", Font.BOLD, 16));
+
         // Default
         setLayout(null);
-        setSize(900,900);
-        setLocation(300,0);
+        setSize(900, 900);
+        setLocation(300, 0);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(false);
         setVisible(true);
