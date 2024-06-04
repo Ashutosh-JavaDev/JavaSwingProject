a26c47550f5fb3b18553d736bbf25263135f3a6c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-04 21:33:40 +0530

    Bank Project

--- BankManagementSystem/src/Deposite.java
+++ BankManagementSystem/src/Deposite.java
@@ -3,11 +3,14 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Deposite {
+public class Deposite extends JFrame implements ActionListener {
     String pinField;
 
     public Deposite(String pinField) {
         this.pinField = pinField;
+    }
+    public void actionPerformed(ActionEvent ae){
+        
     }
 
     public static void main(String[] args) {
