8b8d39254e21378292ae5029ae549550e12110a8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-05 12:37:50 +0530

    Bank Project

--- BankManagementSystem/src/withdraw.java
+++ BankManagementSystem/src/withdraw.java
@@ -12,6 +12,8 @@ public class withdraw extends JFrame implements ActionListener{
         this.pinnumber = pinnumber;
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icons/image.jpg"));
         Image i2=i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        
     }
     public void actionPerformed(ActionEvent ae){
 
