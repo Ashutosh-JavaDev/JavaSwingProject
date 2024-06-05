8901da008937ad488dd09bf2dba0ea945fe0d645
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-05 12:48:19 +0530

    Bank Project

--- BankManagementSystem/src/withdraw.java
+++ BankManagementSystem/src/withdraw.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.Image;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class withdraw extends JFrame implements ActionListener {
-    JTextField withdrawan;
+    JTextField amount;
     JButton drawButton, exit;
     String pinnumber;
 
@@ -13,9 +15,34 @@ public class withdraw extends JFrame implements ActionListener {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/image.jpg"));
         Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel label = new JLabel(i3);
-        label.setBounds(0, 0, 900, 900);
-        add(label);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+        // Field
+        JLabel text = new JLabel("Please enter the Amount want to WithDraw");
+        text.setBounds(160, 320,400, 20);
+        text.setForeground(Color.white);
+        text.setFont(new Font("Arial", Font.BOLD, 16));
+        image.add(text);
+        amount = new JTextField();
+        amount.setBounds(170, 355,300, 25);
+        amount.setForeground(Color.red);
+        amount.setFont(new Font("Arial", Font.BOLD, 12));
+        image.add(amount);
+        // Button
+        drawButton = new JButton("Withdraw");
+        drawButton.setBounds(415,535,100, 25);
+        drawButton.addActionListener(this);
+        // deposit.setForeground(Color.red);
+        drawButton.setFont(new Font("Arial", Font.BOLD, 16));
+        image.add(drawButton);
+        // d
+        exit = new JButton("Exit");
+        exit.setBounds(415,565,100, 25);
+        // deposit.setForeground(Color.red);
+        exit.setFont(new Font("Arial", Font.BOLD, 16));
+        exit.addActionListener(this);
+        image.add(exit);
         // Default
         setSize(900, 900);
         setLocation(300, 0);
