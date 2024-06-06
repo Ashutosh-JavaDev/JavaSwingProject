3d04fa707be090da8e7b287fe6dce58b18bfe9a4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-06 21:32:02 +0530

    Bank Project

--- BankManagementSystem/src/PinChange.java
+++ BankManagementSystem/src/PinChange.java
@@ -4,13 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-public class PinChange {
+public class PinChange extends JFrame implements ActionListener {
     String pinnumber;
     JButton Change,Back;
     JPasswordField pin,repin;
 
     PinChange(String pinnumber){
         this.pinnumber=pinnumber;
+    }
+    public void actionPerformed(ActionEvent ae){
+        
     }
     public static void main(String[]args){
         new PinChange("");
