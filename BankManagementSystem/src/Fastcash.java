4573497d056d3d456400bcbd089a24079ffd3e67
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-06 10:38:27 +0530

    Bank Project

--- BankManagementSystem/src/Fastcash.java
+++ BankManagementSystem/src/Fastcash.java
@@ -6,16 +6,19 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 public class Fastcash extends JFrame implements ActionListener {
     String pinnumber;
 
- public Fastcash(String pinnumber){
+    public Fastcash(String pinnumber) {
 
- }   
- public void actionPerformed(ActionEvent ae){
+    }
 
- }
- public static void main(String[]args){
-    new Fastcash("");
- }
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+
+    public static void main(String[] args) {
+        new Fastcash("");
+    }
 }
