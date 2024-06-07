6fa431b73640a19ed8738b1f603d20f4ef311f5e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-07 12:54:41 +0530

    Bank Project

--- BankManagementSystem/src/miniStatement.java
+++ BankManagementSystem/src/miniStatement.java
@@ -1,3 +1,15 @@
-public class miniStatement {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.awt.Image;
+import java.awt.Color;
+
+public class miniStatement extends JFrame {
+    String pinnumber;
+
+    public miniStatement(String pinnumber) {
+        this.pinnumber = pinnumber;
+    }
+
 }
