40634c476efce507fc8cdcd5d2575123493cfa4a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-07 12:12:35 +0530

    Bank Project

--- BankManagementSystem/src/BalanceEnquiry.java
+++ BankManagementSystem/src/BalanceEnquiry.java
@@ -7,20 +7,22 @@ import java.awt.event.ActionEvent;
 public class BalanceEnquiry extends JFrame implements ActionListener {
     String pinnumber;
     JButton back;
-    public BalanceEnquiry(String pinnumber){
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icons/iimage.jpg"));
-        Image i2=i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel text=new JLabel("");
+
+    public BalanceEnquiry(String pinnumber) {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/iimage.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel text = new JLabel("");
         setLayout(null);
-        setSize(900,900);
-        setLocation(300,0);
+        setSize(900, 900);
+        setLocation(300, 0);
         setUndecorated(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
 
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
         setVisible(false);
         new Transicition(pinnumber).setVisible(true);
     }
