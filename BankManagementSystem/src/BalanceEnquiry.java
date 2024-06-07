1fe3802e383288c9aac3a509a060316c9f341fbb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-07 12:17:17 +0530

    Bank Project

--- BankManagementSystem/src/BalanceEnquiry.java
+++ BankManagementSystem/src/BalanceEnquiry.java
@@ -2,17 +2,31 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
+import java.sql.ResultSet;
 
 public class BalanceEnquiry extends JFrame implements ActionListener {
     String pinnumber;
     JButton back;
 
     public BalanceEnquiry(String pinnumber) {
+        this.pinnumber=pinnumber;
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/iimage.jpg"));
         Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel text = new JLabel("");
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,900,900);
+        add(image);
+        try{
+            BankDataBase conn=new BankDataBase();
+            ResultSet res=conn.statem.executeQuery("select *from login where PinNumber ='"+pinnumber+"'");
+            int balance=0;
+            
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
         setLayout(null);
         setSize(900, 900);
         setLocation(300, 0);
