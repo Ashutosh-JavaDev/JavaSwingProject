b538ea113aec822c34bf77ca1982918c86a4025f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-08 22:07:08 +0530

    ChatApplication

--- ChatApplication/DesktopChatApplication/src/Whatsapp.java
+++ ChatApplication/DesktopChatApplication/src/Whatsapp.java
@@ -3,9 +3,14 @@ import java.awt.Color;
 import java.util.Date;
 import java.awt.Font;
 
-public class Whatsapp {
+public class Whatsapp extends  JFrame {
     public Whatsapp() {
-
+        setLayout(null);
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
-
-}
+    public static void main(String[]args){
+        new Whatsapp();
+    }
+}       
